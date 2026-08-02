dca8b3f52ab844d58e770005892272be91739026
Miguel Rodrigues <62721190+bralisz@users.noreply.github.com>
2026-08-02 00:30:24 -0300

    Delete public directory

--- public/supabase-config.js
+++
@@ -1,5 +0,0 @@
-window.BE_SUPABASE_CONFIG = Object.freeze({
-  url: 'https://cxkevnnxibhezvospkce.supabase.co',
-  publishableKey: 'sb_publishable_yj_yBwVhaUPj7nQdcFDxrg_g_ukcwTX',
-  adminEmail: 'bralisofc@gmail.com'
-});
