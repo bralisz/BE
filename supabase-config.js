fcc0ba83eb469f4b973314e2a5977ef4f0830402
Miguel Rodrigues <62721190+bralisz@users.noreply.github.com>
2026-08-04 08:02:26 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-window.BE_SUPABASE_CONFIG = Object.freeze({
-  url: 'https://cxkevnnxibhezvospkce.supabase.co',
-  publishableKey: 'sb_publishable_yj_yBwVhaUPj7nQdcFDxrg_g_ukcwTX',
-  requireEmailConfirmation: true,
-  oauthAuthorizationPath: '/oauth/consent',
-  oauthAuthorizeEndpoint: 'https://cxkevnnxibhezvospkce.supabase.co/auth/v1/oauth/authorize'
-});
