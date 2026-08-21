5dc4a18d2474381a4b83afa20264da17c6a5a2d4
Miguel Rodrigues <62721190+bralisz@users.noreply.github.com>
2026-08-21 06:24:37 -0300

    Delete config directory

--- config/legal-translations.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = Object.freeze({
-  'en-us': Object.freeze(require('../assets/i18n/en-us.json')),
-  es: Object.freeze(require('../assets/i18n/es.json')),
-  fr: Object.freeze(require('../assets/i18n/fr.json'))
-});
