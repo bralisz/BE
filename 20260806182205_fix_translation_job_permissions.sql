014c04d7cf34349e7dcbeb5b19935c500909a460
Miguel Rodrigues <62721190+bralisz@users.noreply.github.com>
2026-08-23 02:14:00 -0300

    Delete 20260806182205_fix_translation_job_permissions.sql

--- 20260806182205_fix_translation_job_permissions.sql
+++
@@ -1,5 +0,0 @@
-begin;
-grant usage on schema public to service_role;
-grant select, update on public.content_items to service_role;
-grant select, update on public.site_settings to service_role;
-commit;
